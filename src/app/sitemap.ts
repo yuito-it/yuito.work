042a4e1c120a68ebaad1c3d8e9777ee2c4666435
Yuito Akatsuki (Tani Yutaka) <yuito@yuito.work>
2025-02-24 10:00:57 +0900

    サイトマップに「works」ルートを追加し、英語および日本語のルートを統一

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,23 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const routes = ["", "about", "contacts", "ja", "ja/about", "ja/contacts"].map(
-    (route) => ({
-      url: `https://www.yuito-it.jp/${route}`,
-      lastModified: new Date().toISOString().split("T")[0],
-    })
-  );
+  const routes = [
+    "",
+    "about",
+    "contacts",
+    "works",
+    "ja",
+    "ja/about",
+    "ja/contacts",
+    "ja/works",
+    "en",
+    "en/about",
+    "en/contacts",
+    "en/works",
+  ].map((route) => ({
+    url: `https://www.yuito-it.jp/${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
   return [...routes];
 }

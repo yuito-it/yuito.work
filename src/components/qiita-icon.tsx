39a3625ac13b6fd64f9ece973dcd803ecf102493
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-05-07 21:12:07 +0900

    フォーマット

--- src/components/qiita-icon.tsx
+++ src/components/qiita-icon.tsx
@@ -3,24 +3,40 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function QiitaIcon() {
-    const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("light");
 
-    useEffect(() => {
-        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-        const handleChange = (e: MediaQueryListEvent) => {
-            setTheme(e.matches ? "dark" : "light");
-        };
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (e: MediaQueryListEvent) => {
+      setTheme(e.matches ? "dark" : "light");
+    };
 
-        setTheme(mediaQuery.matches ? "dark" : "light");
-        mediaQuery.addEventListener("change", handleChange);
+    setTheme(mediaQuery.matches ? "dark" : "light");
+    mediaQuery.addEventListener("change", handleChange);
 
-        return () => {
-            mediaQuery.removeEventListener("change", handleChange);
-        };
-    }, []);
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
 
-    if (theme === "dark") {
-        return (<Image src={"/img/qiita-white.webp"} className="rounded-full" width="15" height="15" alt={""} />);
-    }
-    return (<Image src={"/img/qiita-black.webp"} className="rounded-full" width="15" height="15" alt={""} />);
+  if (theme === "dark") {
+    return (
+      <Image
+        src={"/img/qiita-white.webp"}
+        className="rounded-full"
+        width="15"
+        height="15"
+        alt={""}
+      />
+    );
+  }
+  return (
+    <Image
+      src={"/img/qiita-black.webp"}
+      className="rounded-full"
+      width="15"
+      height="15"
+      alt={""}
+    />
+  );
 }

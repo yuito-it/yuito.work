6656d621b026a339003690ddd81c4ac29bea34d9
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-12-08 11:33:35 +0900

    パスのミス

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -2,8 +2,8 @@ import { Locale } from "@/i18n-config";
 import "server-only";
 
 const dictionaries = {
-  en: () => import("../../dictData/en.js").then((module) => module.default),
-  ja: () => import("../../dictData/ja.js").then((module) => module.default),
+  en: () => import("../../dictData/en.jsx").then((module) => module.default),
+  ja: () => import("../../dictData/ja.jsx").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) =>
